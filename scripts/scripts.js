9546d144b07973f75e9129b33be02f64c72d6ee4
WilliamRincon02 <rinconwilliam996@gmail.com>
2024-01-18 12:17:14 -0500

    Commit 18/01/2023

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,15 @@ const fecha = document.querySelector('#fecha');
 const lista = document.querySelector('#lista');
 const input = document.querySelector('#input-tarea');
 const botonEnter = document.querySelector('#enter');
+const id = 0;
 
 //funcion agregar tarea
 
-function agregarTarea (tarea){
+function agregarTarea (tarea, id, realizado, eliminado){
     const elemento = `<li id="elemento">
-                    <i id="0" data="realizado" class="fa-regular fa-circle-dot"></i>
+                    <i id="${id}" data="${realizado}" class="fa-regular fa-circle-dot"></i>
                     <p class="text">${tarea}</p>
-                    <i id="0" data="elimidado" class="fa-solid fa-trash"></i>
+                    <i id="0" data="${eliminado}" class="fa-solid fa-trash"></i>
                     </li>`;
 
     lista.insertAdjacentHTML("afterbegin", elemento)
@@ -19,18 +20,22 @@ botonEnter.addEventListener('click', ()=>{
     const tarea = input.value;
 
     if(tarea) {
-        agregarTarea(tarea);
+        agregarTarea(tarea, id, false, false);
     }
     input.value = '';
+    id++;
 })
+
+//funcion agregar tarea con tecla enter
 
 document.addEventListener('keyup', function(event){
     if(event.key == 'Enter'){
         const tarea = input.value;
 
         if(tarea) {
-            agregarTarea(tarea);
+            agregarTarea(tarea, id, false, false);
         }
         input.value = "";
+        id++;
     }
 })
